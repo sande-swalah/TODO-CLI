bcf838b0d43b41c7bccb2f5cf49affb42b600520
sande-swalah <alvinswalah@gmail.com>
2026-03-01 22:02:21 +0300

    Added decorators to the task and users (#8)
    * added decorators to the task and users
    
    * fixing some bugs

--- models/user.py
+++ models/user.py
@@ -2,10 +2,56 @@ import hashlib
 
 class User:
     def __init__(self, name, email, password, role="user"):
+
         self.name = name
         self.email = email
-        self.password = hashlib.md5(password.encode()).hexdigest()
+        self.password = password      
         self.role = role
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+    
+        if not value or not isinstance(value, str):
+            raise ValueError("Name must be a non-empty string")
+        self._name = value
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if not value or not isinstance(value, str) or "@" not in value:
+            raise ValueError("Invalid email format")
+        self._email = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        if not value or not isinstance(value, str):
+            raise ValueError("Password must be a string")
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters")
+        
+        self._password = hashlib.md5(value.encode()).hexdigest()
+    @property
+    def role(self):
+        return self._role
+
+    @role.setter
+    def role(self, value):
+        valid_roles = ["user", "admin", "moderator"]
+        if value not in valid_roles:
+            raise ValueError(f"Role must be one of {valid_roles}")
+        self._role = value
+
 
     def check_password(self, password):
         return self.password == hashlib.md5(password.encode()).hexdigest()
