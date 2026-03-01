795a297a33a6282c65d6c14d319f30d85d0600bf
sande-swalah <alvinswalah@gmail.com>
2026-03-01 14:12:59 +0300

    Tasks and the user platform (#3)
    * Adding the task function to the code
    
    * task module complete
    
    * fixing some of the issues
    
    * tasks done

--- models/user.py
+++ models/user.py
@@ -1,3 +1,19 @@
+import hashlib
 
+class User:
+    def __init__(self, name, email, password, role="user"):
+        self.name = name
+        self.email = email
+        self.password = hashlib.md5(password.encode()).hexdigest()
+        self.role = role
 
+    def check_password(self, password):
+        return self.password == hashlib.md5(password.encode()).hexdigest()
 
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+            "role": self.role
+        }
