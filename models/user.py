2ab5357b1e23c7d31f4ed03b070b376ec391fed0
Cewa Mahan <cewa.mahan@student.moringaschool.com>
2026-03-01 14:10:50 +0300

    Add password hashing to User model

--- models/user.py
+++ models/user.py
@@ -1,3 +1,11 @@
+import hashlib
+
+class User:
+    def __init__(self, name, email, password, role="user"):
+        self.name = name
+        self.email = email
+        self.password = hashlib.md5(password.encode()).hexdigest()
+        self.role = role
 
 
 
